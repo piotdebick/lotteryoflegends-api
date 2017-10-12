271988b2690ced99655c47f62da834245e3a338e
Piotr <piotr.debicki@cix.csi.cuny.edu>
2017-10-12 16:59:00 -0400

    added Auth header to access control

--- server/server.js
+++ server/server.js
@@ -17,8 +17,7 @@ var codeRoute = require('./routes/codeRoute');
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, x-auth');
-    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Headers', 'Authorization, Content-Type, x-auth');
     res.header('Access-Control-Expose-Headers', 'x-auth');
     next();
 }
